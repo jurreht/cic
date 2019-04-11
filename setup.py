44d43b55ddf4537e174e8508254b8b3971e4efb4
Jurre Thiel <xs2jrr@xs4all.nl>
2019-04-11 14:59:23 +0200

    Skeleton package structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='cic',
+    version='0.1.0',
+    author='Jurre H. Thiel',
+    author_email='j.h.thiel@vu.nl',
+    packages=['cic'],
+    license='LICENSE',
+    description='A library to compute the Changes-In-Changes estimator',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'numpy >= 1.16.1',
+    ]
+)
